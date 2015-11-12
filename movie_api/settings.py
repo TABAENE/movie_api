fe3b27d7ec336087911b790032ec9450be692a02
thaque <tabaene.haque786@gmail.com>
2015-11-12 19:50:44 +0530

    fifth commit

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'movie_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["/home/thaque/movie_api/movie/static/templates"],
+        'DIRS': [os.path.join(BASE_DIR, "movie/static/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
