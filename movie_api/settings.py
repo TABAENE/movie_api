148e64e193fb8d38599ff6b3bd3fcb0bfd728f18
thaque <tabaene.haque786@gmail.com>
2015-11-13 00:38:35 +0530

    eleventh commit

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/assets/js/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "movie/assets")
 
 CORS_ORIGIN_ALLOW_ALL = True
