79ea300dd09872ae7c8d649c3c9c1580112b0e6e
thaque <tabaene.haque786@gmail.com>
2015-11-13 11:42:18 +0530

    second day sixth

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -109,7 +109,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	os.path.join(PROJECT_DIR, 'movie/static'),
+	os.path.join(BASE_DIR, 'movie/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
