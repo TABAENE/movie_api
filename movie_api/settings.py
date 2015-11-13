efcb89005cff466d3b79bc14d838b075272f1d0a
thaque <tabaene.haque786@gmail.com>
2015-11-13 10:55:57 +0530

    second day commit

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 WSGI_APPLICATION = 'movie_api.wsgi.application'
 
 
@@ -104,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "movie/assets")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = (
@@ -122,9 +124,9 @@ REST_FRAMEWORK = {
     )
 }
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "movie/assets/js"),
-#)
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
 
 ALLOWED_HOSTS = [
     'mysterious-mesa-2167.herokuapp.com', 
